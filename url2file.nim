2469b9684d949b06c726bef9cd005b7acd33e59c
nivrrex <nivrrex@gmail.com>
2023-03-24 09:43:10 +0800

    Add "--help" "--version" command line arguments
    1.Optimize the implementation of command line parameters
    2.Add "--help" "--version" command line arguments

--- url2file.nim
+++ url2file.nim
@@ -6,23 +6,29 @@ var url = ""
 var out_file = ""
 var content = ""
 
-let args = commandLineParams()
-var p = initOptParser(args)
-while true:
-  p.next()
-  case p.kind
-  of cmdEnd: break
-  of cmdShortOption, cmdLongOption:
-    if p.key == "url" or p.key == "u" :
-      url = p.val
-    elif p.key == "out" or p.key == "o" :
-      out_file = p.val
-    elif p.val == "":
-      echo "Option is null ", p.key
-    else:
-      echo "Option and value: ", p.key, ", ", p.val
+for kind, key, value in getOpt():
+  case kind
   of cmdArgument:
-    echo "Argument: ", p.key
+    echo "# command line argument : \"", key, "\""
+  of cmdLongOption, cmdShortOption:
+    case key
+    of "h", "help":
+      echo "url2file.exe -u=http://test.url -o=test.txt"
+      echo "url2file.exe --url=http://test.url --out=test.txt"
+      quit()
+    of "v", "version":
+      echo getAppFilename().extractFilename()
+      echo "version: 0.1.0"
+      echo "author: nivrrex"
+      quit()
+    of "u", "url":
+      url = value
+    of "o", "out":
+      out_file = value
+    else:
+      echo "Unknown option: ", key
+  of cmdEnd:
+    discard
 
 var client = newHttpClient()
 if url != "":
