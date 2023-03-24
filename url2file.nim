d4e12304d15c0d10f241cb74796e6a44e88989bc
nivrrex <nivrrex@gmail.com>
2023-03-24 09:45:44 +0800

    Add the "User-Agent" header

--- url2file.nim
+++ url2file.nim
@@ -31,6 +31,7 @@ for kind, key, value in getOpt():
     discard
 
 var client = newHttpClient()
+client.headers = newHttpHeaders({ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36" })
 if url != "":
     content = client.getContent(url)
     if out_file != "":
